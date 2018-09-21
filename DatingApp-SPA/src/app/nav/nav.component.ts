ab29a3560773dc3968e6522bdf02f755c4796a4c
artyomv14 <artyom14@gmail.com>
2018-09-21 10:47:14 +0300

    global error handling

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -19,7 +19,7 @@ export class NavComponent implements OnInit {
         console.log('Logged in successfully');
       },
       err => {
-        console.log('Failed to login');
+        console.log(err);
       }
     );
   }
